7921e89792547a9f3c01013d1e1eab21f0e9ce4d
Joakim <cloromakasol@hotmail.com>
2024-01-03 11:34:25 +0100

    A menu and error handling are added to the program.

--- Program.cs
+++ Program.cs
@@ -4,22 +4,58 @@ namespace Fibonacci
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Enter a number to generate the Fibonacci sequence:");
-            BigInteger number = (BigInteger)Convert.ToInt64(Console.ReadLine());
-            memo = new BigInteger[(int)number];
-            Console.WriteLine("===================================================================================");
-            for (BigInteger i = 0; i < number; i++)
+            bool isRunning = true;
+
+            while (isRunning)
             {
-                Console.WriteLine("{0})", i + 1);
-                Console.WriteLine("Fibonacci no: " + Fibonacci(i).ToString("N0"));
-                Console.WriteLine("-----------------------------------------------------------------------------------");
+                Console.WriteLine("\n\t GENERATE THE FIBONACCI SEQUENCE");
+                Console.Write("\n\t[1]Enter program");
+                Console.WriteLine("\n\t[2]Quit program");
+
+                try
+                {
+                    int menuChoice = Convert.ToInt32(Console.ReadLine());
+
+                    if (menuChoice != 1)
+                    {
+                        Console.WriteLine("\nYou can only choose either number 1 or 2.");
+                    }
+
+                    switch (menuChoice)
+                    {
+                        case 1:
+
+                            Console.WriteLine("Enter a number to generate the Fibonacci sequence:");
+                            BigInteger number = (BigInteger)Convert.ToInt64(Console.ReadLine());
+                            memo = new BigInteger[(int)number];
+                            Console.WriteLine("===================================================================================");
+
+                            for (BigInteger i = 0; i < number; i++)
+                            {
+                                Console.WriteLine("{0})", i + 1);
+                                Console.WriteLine("Fibonacci no: " + Fibonacci(i).ToString("N0"));
+                                Console.WriteLine("-----------------------------------------------------------------------------------");
+                            }
+                            Console.ReadLine();
+
+                            break;
+
+                        case 2:
+                            isRunning = false;
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("\n\tWrong input format. Returning to the menu.");
+                    continue;
+                }
             }
-            Console.ReadLine();
         }
 
-        private static BigInteger[] memo;
+        private static BigInteger[]? memo;
 
         private static BigInteger Fibonacci(BigInteger n)
         {
